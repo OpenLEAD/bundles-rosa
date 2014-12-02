d0d6fc033ed0d186167fe9d357619f6598b69b2f
alcantara09 <alcantara@poli.ufrj.br>
2014-12-02 12:56:08 -0200

    Changes in the localization with replay script
    Moving to bundles and trying to correct the transformer.
    
    The sonar component been replayed doesn't support the port.frame

--- scripts/localization_with_replay.rb
+++ scripts/localization_with_replay.rb
@@ -1,89 +1,77 @@
-class FilterConfig
-    def initialize seed = 42
-        @seed = seed
-	@config = {}
-    end
-
-    def localization
-        c = @config[:rosa]
-        c.seed = @seed
-        c.particleCount = 250
-        c.minEffective = 50 
-        c.initialRotationError = Eigen::Vector3.new(0, 0, 0.1)
-        c.initialTranslationError = Eingen::Vector3.new(0.1, 0.1, 1.0)
-        c.initialRollError =  0.1 
-        c.discountFactor = 0.9 
-        c.spreadThreshold = 0.9
-        c.spreadTranslationXFactor = 0.1
-        c.spreadTranslationYFactor = 0.1
-        c.spreadTranslationZFactor 0.1 
-        c.maxYawDeviation = 15*M_PI/180.0
-        c.measurementThreshold = 0.1, 10*M_PI/180.0 
-        c.maxSensorRange = 3.0
-        c.logDebug = false 
-        c.logParticlePeriod =  100 
-
-        c = @config[:odometry]
-        c.seed = @seed
-        c.constError.translation = Eigen::Vector3.new( 0.01, 0.01, 0.0 )
-        c.constError.yaw = 0.005 
-        c.distError.translation = Eigen::Vector3.new( 0.1, 0.5, 0.0 )
-        c.distError.yaw = 0.001 
-        c.tiltError.translation = Eigen::Vector3.new( 0.01, 0.01, 0.0 )
-        c.tiltError.yaw = 0.001
-        c.dthetaError.translation = Eigen::Vector3.new( 0.05, 0.01, 0.0 )
-        c.dthetaError.yaw = 0.005
-    end
-end
+require 'rock/bundles'
+require 'orocos'
+require 'readline'
+#library for displaying data
+require 'vizkit'
 
 
 # You need to initialize before you can use the transformer
-Orocos.initialize
-Orocos.transformer.load_conf('config/transforms.rb')
-Orocos.run ... do
-    filter = Orocos.name_service.get 'rosa_localization'
+include Bundles
+Bundles.initialize
+Bundles.transformer.load_conf('config/transforms.rb')
+Bundles.run 'rosa_localization::Task' => 'rosa_localization' do
+    rosa_localization = Orocos.name_service.get 'rosa_localization'
 
     #Open the log of the components necessary to run the localizartion
     #It is assumed that the script is going to be run on the log folder 
     #of the FILTERED logs
-    sonar_log = Orocos::Log::Replay.open("micron.0.log")
-    ptu_log =  Orocos::Log::Replay.open("ptu.0.log")
+    sonar_log = Orocos::Log::Replay.open("../logs/20141106-1455-Micron180to0aprox6.3m/micron.0.log")
+    ptu_log =  Orocos::Log::Replay.open("../logs/20141106-1455-Micron180to0aprox6.3m/ptu.0.log")
+
+    #
+    sonar = sonar_log.micron
+    ptu = ptu_log.ptu
     
+    ## Create a sample writer for the pressure sensor port ##
+    depth_samples_port = rosa_localization.depth.writer
+
+    #Create a sample 
+    depth_sample = depth_samples_port.new_sample
+    depth_sample.position[0] = 0
+    depth_sample.position[1] = 0
+    #Try to match the value from the current log
+    depth_sample.position[2] = 6.0
+
+    depth_sample.orientation = Eigen::Quaternion.Identity
+
+    # Tell in which frames is the data expressed. These are not
+    # arbitrary anymore: they MUST match the frame names listed
+    # in the transformer's
+
+    #The log is not allowed to have this proprety set.
+    sonar.sonar_beam.frame = 'sonar'
+
+    # Properly setup frame names. These are not arbitrary anymore:
+    # they MUST match the frame names listed in the transformer's
+    # configuration files
+    rosa_localization.body_frame = "body"
+
+    #The ptu frames are already set as the default values in the log
+    #ptu.base_frame = "ptu_kongsberg_base"
+    #ptu.moving_frame = "ptu_kongsberg_moving"
+ 
+    sonar.sonar_beam.connect_to(rosa_localization.sonarBeams)
+    
+    # Finalize transformer configuration (see below for explanations)
+    # For static transformations the task should not not yet be configured
+    Orocos.transformer.setup(rosa_localization,ptu,sonar)
+
+    rosa_localization.configure
+
+    #open control widget and start replay
+    Vizkit.control sonar_log
+    Vizkit.control ptu_log
+    Vizkit.exec
+
+    rosa_localization.start
+    #To run all the replays at once with normal speed
+    #replay.run
     
 
-  # Tell in which frames is the data expressed. These are not
-  # arbitrary anymore: they MUST match the frame names listed
-  # in the transformer's
-  sonar_log.sonar_beam.frame = 'sonar'
-  #filter.filtered_samples.frame = ''
-
-  # Properly setup frame names. These are not arbitrary anymore:
-  # they MUST match the frame names listed in the transformer's
-  # configuration files
-  filter.body_frame = "body"
-  ptu.base_frame = "ptu_kongsberg_base"
-  servo.moving_frame = "ptu_kongsberg_moving"
-
-  sonar_log.micron.sonar_beam.sonar_beam.connect_to(filter.sonarBeams)
-  	
-
-  # Finalize transformer configuration (see below for explanations)
-  # For static transformations the task should not not yet be configured
-  Orocos.transformer.setup(sonar_log, filter, ptu_log)
-
-
-  filter.rosa_localization_config = Config.new 42
-  filter.configure
-  filter.start
   
-  #open control widget and start replay
-  Vizkit.control log
-  Vizkit.exec
+    Readline::readline("Press ENTER to exit\n") do
+    end 
 
-  
-  Readline::readline("Press ENTER to exit\n") do
-  end 
-
-  # Wait for ENTER on input
-  STDIN.readline
+    # Wait for ENTER on input
+    STDIN.readline
 end
