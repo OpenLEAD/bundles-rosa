fbfe3795df36ed0643684ee685d6d771818cbcea
alcantara09 <alcantara@poli.ufrj.br>
2015-01-22 11:47:04 -0200

    Correction of the transformation chain ( surface to seaking trasnducer)

--- config/transforms.rb
+++ config/transforms.rb
@@ -3,18 +3,23 @@ require 'eigen'
 # Mounting angle between the PTU's tilt plate and the mounting bracket
 TILT_PLATE_TO_MOUNTING_BRACKET_ANGLE = - Math::PI / 4
 
+dynamic_transform 'pressure.depth_samples',
+    'surface' => 'body_horizontal'
+dynamic_transform 'inclination_body.roll_samples',
+    'body' => 'body_horizontal'
+
 # The PTU is mounted upside down
-static_transform Eigen::Vector3.new(0, 0, 1), Eigen::Quaternion.from_angle_axis(Math::PI, Eigen::Vector3.UnitX),
-    'body' => 'ptu_pan_plate'
+static_transform Eigen::Vector3.new(0, 0, -1),
+    'ptu_pan_plate' => 'body'
 
 dynamic_transform 'ptu.transformation_samples',
-    'ptu_pan_plate' => 'ptu_tilt_plate'
+    'ptu_tilt_plate' => 'ptu_pan_plate'
 
 # Transformation that accounts for the mounting angle between the mounting
 # bracket and the PTU's tilt plate
 static_transform Eigen::Vector3.new(-0.103, 0, 0.078),
-    'ptu_tilt_plate' => 'ptu_mounting_bracket_plate'
+    'ptu_mounting_bracket_plate' => 'ptu_tilt_plate'
 
-static_transform Eigen::Vector3.new(-0.045, -0.105, 0), Eigen::Quaternion.from_angle_axis(Math::PI, Eigen::Vector3.UnitZ) * Eigen::Quaternion.from_angle_axis(-Math::PI, Eigen::Vector3.UnitX),
-    'ptu_mounting_bracket_plate' => 'seaking_transducer'
+static_transform Eigen::Vector3.new(-0.045, -0.105, 0), Eigen::Quaternion.from_angle_axis(-Math::PI / 2, Eigen::Vector3.UnitY) * Eigen::Quaternion.from_angle_axis(-Math::PI / 2, Eigen::Vector3.UnitZ),
+    'seaking_transducer' => 'ptu_mounting_bracket_plate'
 
