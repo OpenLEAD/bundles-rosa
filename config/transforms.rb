0361cda6c114d70a91324333be0b07899d6487ca
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-01-28 14:25:06 -0200

    go back to the ROSA transformer setup

--- config/transforms.rb
+++ config/transforms.rb
@@ -1,28 +1,16 @@
-dynamic_transform 'scan_aligner.transform_samples',
-    'scan_ref' => 'surface_center'
+require 'eigen'
 
-static_transform Eigen::Quaternion.from_angle_axis(-Math::PI/2, Eigen::Vector3.UnitZ),
-    'surface' => 'scan_ref'
+# Mounting angle between the PTU's tilt plate and the mounting bracket
+TILT_PLATE_TO_MOUNTING_BRACKET_ANGLE = - Math::PI / 4
 
 dynamic_transform 'pressure.depth_samples',
-    'surface' => 'world_inclinometer_roll'
+    'surface' => 'body_horizontal'
 dynamic_transform 'inclination_body.roll_samples',
-    'inclinometer_roll' => 'world_inclinometer_roll'
+    'body' => 'body_horizontal'
 
-#Static transformation for the inclinomenter be a pitch
-static_transform Eigen::Quaternion.from_angle_axis(Math::PI / 2, Eigen::Vector3.UnitZ),
-    'world_inclinometer_pitch' => 'inclinometer_roll'
-
-dynamic_transform 'inclination_body_2.roll_samples',
-    'inclinometer_pitch' => 'world_inclinometer_pitch'
-
-#Back to the body frame
-static_transform Eigen::Quaternion.from_angle_axis(-Math::PI / 2, Eigen::Vector3.UnitZ),
-    'body' => 'inclinometer_pitch'
-    
-## The PTU is mounted upside down
-static_transform Eigen::Vector3.new(0.3, 0.3, -0.5), Eigen::Quaternion.from_angle_axis(-Math::PI/2, Eigen::Vector3.UnitY) * Eigen::Quaternion.from_angle_axis(Math::PI, Eigen::Vector3.UnitX) * Eigen::Quaternion.from_angle_axis(-Math::PI/2, Eigen::Vector3.UnitZ),
-    'body' => 'ptu_tilt_plate'
+# The PTU is mounted upside down
+static_transform Eigen::Vector3.new(0, 0, -1),
+    'ptu_pan_plate' => 'body'
 
 dynamic_transform 'ptu.transformation_samples',
     'ptu_tilt_plate' => 'ptu_pan_plate'
@@ -30,8 +18,8 @@ dynamic_transform 'ptu.transformation_samples',
 # Transformation that accounts for the mounting angle between the mounting
 # bracket and the PTU's tilt plate
 static_transform Eigen::Vector3.new(-0.103, 0, 0.078),
-    'ptu_mounting_bracket_plate' => 'ptu_pan_plate'
+    'ptu_mounting_bracket_plate' => 'ptu_tilt_plate'
 
-static_transform Eigen::Vector3.new(-0.045, -0.105, 0), Eigen::Quaternion.from_angle_axis(Math::PI / 2, Eigen::Vector3.UnitX) * Eigen::Quaternion.from_angle_axis(Math::PI, Eigen::Vector3.UnitZ),
+static_transform Eigen::Vector3.new(-0.045, -0.105, 0), Eigen::Quaternion.from_angle_axis(-Math::PI / 2, Eigen::Vector3.UnitY) * Eigen::Quaternion.from_angle_axis(-Math::PI / 2, Eigen::Vector3.UnitZ),
     'seaking_transducer' => 'ptu_mounting_bracket_plate'
 
